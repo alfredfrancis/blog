30c8928d148dac5aa57754244f6004904921a0a8
alfred francis <alfredfranciz@gmail.com>
2015-12-22 14:57:28 +0530

    added scope,validation

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,8 +2,8 @@ class Article < ActiveRecord::Base
 	has_many :comments, dependent: :destroy
 	validates :title, presence: true,
                     length: { minimum: 5 }
-                    
-  def self.search(query)
-    where("title like ?", "%#{query}%") 
-  end
+    validates_uniqueness_of :title
+
+scope :search, ->(query) { where("title like ?", "%#{query}%") }
+
 end
